e606aa3ac8da3d98fcdf9e3e2835e60d90ee376e
rfrancian <info@vraven.us>
2019-05-08 13:27:15 -0700

    pruning unused items

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var renderRH = require('./renderthisRH');
-var renderLH = require('./renderthisLH');
-
-
-
-
 
 
 
@@ -14,15 +8,6 @@ var renderLH = require('./renderthisLH');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Render farm manager v.001' });
 });
-
-
-
-console.log(filename);
-console.log(samples);
-
-
-renderRH(filename,samples);
-renderLH(filename,samples);
 
 
 
