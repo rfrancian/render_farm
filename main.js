679a240beeb6e0b9efd2627217560ed08e8bced6
rfrancian <info@vraven.us>
2019-05-16 13:44:32 -0700

    organization tweaks

--- main.js
+++ main.js
@@ -10,17 +10,8 @@ const workerNode1 = 'rsf@192.168.86.20'
 // for the pi branch we use the NAS scripts
 const scriptnameNode1 = '/Volumes/blenderFiles/blenderScript_Node1'
 const scriptnameNode2 = '/Volumes/blenderFiles/blenderScript_Node2'
-// const scriptnameNode1 = '/home/rsf/render_farm/blenderScript2.8_Node1'
-// const scriptnameNode2 = '/home/rsf/render_farm/blenderScript2.8_Node2'
-
-// using local script here because I'm testing this code on one of the machines that's
-// doing the rendering so I have to use sshpass to logon to itself.
-// the normal render_farm server will use the uncommented script above
-// const scriptnameNode2 = './blenderScript_Node2'
-
 
 // blender file name and requested samples
-// const filename = '/Volumes/blenderFiles/flowers2.79.blend'
 const filename = '/Volumes/blenderFiles/statueDecimatedUV3packed.blend'
 
 
@@ -34,8 +25,6 @@ const maxX_Node1 = split;
 const minX_Node2 = split;
 const maxX_Node2 = 1.0;
 
-
-
 // 
 console.log(workerNode2);
 console.log(workerNode1);
@@ -44,6 +33,7 @@ console.log(filename);
 console.log(samples);
 
 // render left and right sides of image
-renderNode2(scriptnameNode2, filename, samples, workerNode2, minX_Node2, maxX_Node2);
 renderNode1(scriptnameNode1, filename, samples, workerNode1, minX_Node1, maxX_Node1);
+renderNode2(scriptnameNode2, filename, samples, workerNode2, minX_Node2, maxX_Node2);
+
 
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Render the farm manager v.001' });
+  res.render('index', { title: 'Render, the farm manager v.001' });
 });
 
 
